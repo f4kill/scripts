dff6ce5157627815d0f274be22e2180ac98c3b57
Aymeric Bianco Pelle <biancop.aymeric@icloud.com>
2019-02-24 22:55:04 +0100

    Added dynhost cron task

---
+++ dynhost.sh
@@ -0,0 +1,53 @@
+#/bin/sh
+ 
+#
+# CONFIG
+#
+ 
+HOSTS=( home.mrik.pw ftp.mrik.pw )
+LOGIN=mrik.pw-home
+PASSWORD=Yu8!8lrYeVkt
+ 
+PATH_LOG=/var/log/dynhost
+ 
+#
+# GET IPs
+#
+ 
+CURRENT_IP=`curl -4 ifconfig.co`
+ 
+#
+# LOG
+#
+echo > $PATH_LOG
+echo -n "Run dyndns " >> $PATH_LOG
+date >> $PATH_LOG
+ 
+echo -n "Current IP " >> $PATH_LOG
+echo "$CURRENT_IP" >> $PATH_LOG
+for i in "${HOSTS[@]}"
+do
+   : 
+	HOST_IP=`dig +short $i`
+
+	echo -n "[$i] IP: " >> $PATH_LOG
+	echo "$HOST_IP" >> $PATH_LOG
+	 
+	#
+	# DO THE WORK
+	#
+	if [ -z $CURRENT_IP ] || [ -z $HOST_IP ]
+	then
+	        echo "[$i] No IP retrieved" >> $PATH_LOG
+	else
+	        if [ "$HOST_IP" != "$CURRENT_IP" ]
+	        then
+	                echo "[$i] IP has changed" >> $PATH_LOG
+	                RES=`curl --user "$LOGIN:$PASSWORD" "https://www.ovh.com/nic/update?system=dyndns&hostname=$HOST&myip=$CURRENT_IP"`
+	                echo -n "[$i] Request result : " >> $PATH_LOG
+	                echo "$RES" >> $PATH_LOG
+	        else
+	                echo "[$i] IP has not changed" >> $PATH_LOG
+	        fi
+	fi
+done
